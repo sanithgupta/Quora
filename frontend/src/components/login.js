c2f11f038f1e16dfea52c7b7430296c8e12b9077
Naveen Ravipati <naveenravipati7gmail.com>
2019-04-20 04:09:05 -0700

    Completed frontend for Signup

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -11,43 +11,105 @@ import { stat } from 'fs';
 /* REDUX IMPORTS END */
 
 class login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            temp: true
+        }
+    }
+
+    signin = (e) => {
+        this.setState({
+            temp: false
+        });
+    }
+
+    cancel = (e) => {
+        this.setState({
+            temp: true
+        });
+    }
+
     render() {
+        if (this.state.temp) {
+            var a =
+                <div>
+                    <div>
+                        <button class="btn google-btn social-btn " type="button"><span><i class="fab fa-google-plus-g"></i> Sign in with Google+</span> </button>
+                    </div>
+                    <div>
+                        <button class="btn facebook-btn social-btn" type="button"><span><i class="fab fa-facebook-f"></i> Sign in with Facebook</span> </button>
+                    </div>
+                    <div style={{ textAlign: 'left', color: '#999999' }}>
+                        <p>
+                            <a style = {{cursor:'pointer',color:'#007BFF'}} onClick={this.signin}>Sign up with Email.</a>
+                            &nbsp;By signing up you <br></br>indicate that you have read and agree to <br></br> Quora's&nbsp;
+                        <a href='#'>Terms of Service</a>
+                            &nbsp;and&nbsp;
+                        <a href='#'>Privacy Policy</a>
+                        </p>
+                    </div>
+                </div>
+        }
+        else {
+            var a =
+                <div class = 'container'>
+                    <form>
+                        <label style={{ color: '#999999' }}><strong>Sign Up</strong></label><br></br>
+                        <div class = 'row'>
+                            <label style={{ color: '#999999' }} class = 'col' for ='first_name'> <strong>First Name</strong></label>
+                            <label style={{ color: '#999999' }} class = 'col' for ='last_name'><strong>Last Name</strong></label>
+                        </div>
+                        <div class = 'row'>
+                            <input class = 'col col-md-5 form-control'  type='text' id = 'first_name'></input>
+                            <input class = 'col col-md-5 offset-1 form-control'  type='text' id = 'last_name'></input>
+                        </div>
+                        <div class = 'row'>
+                            <label style={{ color: '#999999' }} for='email'><strong>Email</strong></label>
+                            <input class='form-control' type='text' id = 'email'></input>
+                        </div>
+                        <div class = 'row'>
+                            <label style={{ color: '#999999' }} for='password'><strong>Password</strong></label>
+                            <input class='form-control' type='password' id = 'password'></input>
+                        </div>
+                        <div>
+                            <p style={{ color: '#999999'}}>
+                            By clicking "Sign Up" you indicate that you &nbsp;
+                            have read and agree to Quora's<a href = '#'>Terms of Service</a> and&nbsp;
+                            <a href = '#'>Privacy Policy</a>
+                            </p>
+                        </div>
+                        <div style = {{textAlign:'right', marginBottom:'5%'}}>
+                            <a style={{ color: '#999999'}} onClick={this.cancel}>Cancel</a>&nbsp;&nbsp;
+                            <button class = 'btn btn-primary'>Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+        }
+
+
         return (
             <div class="Login-component">
                 <div>
                     <div class='login-form container'>
-                        <div class='heading-block col col-sm-12'>
-                            <div style = {{textAlign:'center',marginBottom:'3%',marginTop:'5%'}}>
-                                <img src = 'http://qsf.fs.quoracdn.net/-3-images.logo.wordmark_default.svg-26-bfa6b94bc0d6af2e.svg'></img>
+                        <div class='heading-block col col-md-12'>
+                            <div style={{ textAlign: 'center', marginBottom: '1%', marginTop: '3%' }}>
+                                <img src='http://qsf.fs.quoracdn.net/-3-images.logo.wordmark_default.svg-26-bfa6b94bc0d6af2e.svg'></img>
                             </div>
                             <p class='paragraph' >A place to share knowledge and better understand the world</p>
                         </div>
-                        <div class="col col-sm-12" style={{ marginBottom: '2%' }}>
-                            <div className='col col-sm-6'>
-                                <div>
-                                    <button class="btn google-btn social-btn " type="button"><span><i class="fab fa-google-plus-g"></i> Sign in with Google+</span> </button>
-                                </div>
-                                <div>
-                                    <button class="btn facebook-btn social-btn" type="button"><span><i class="fab fa-facebook-f"></i> Sign in with Facebook</span> </button>
-                                </div>
-                                <div style={{ textAlign: 'left', color: '#999999' }}>
-                                    <p>
-                                        <a href='#'>Sign up with Email.</a>
-                                        &nbsp;By signing up you <br></br>indicate that you have read and agree to <br></br> Quora's&nbsp;
-                                        <a href='#'>Terms of Service</a>
-                                        &nbsp;and&nbsp;
-                                        <a href='#'>Privacy Policy</a>
-                                    </p>
-                                </div>
+                        <div className="row">
+                            <div className='col'>
+                                {a}
                             </div>
-                            <div className='col col-sm-6'>
+                            <div className='col'>
                                 <div style={{ textAlign: 'left', marginBottom: '10%' }}>
-                                <form>
-                                    <label>Login</label><br></br>
-                                    <input class = "form-control" style={{ width: '100%', marginBottom: '5%' }} type='text' placeholder='Email'></input>
-                                    <input  class = "form-control" style={{ width: '100%', marginBottom: '3%' }} type='password' placeholder='Password'></input>
-                                    <a style={{ marginRight: '35%', color: '#999999' }} href='#'>Forgot Password?</a>
-                                    <button class='btn btn-primary' type='submit'>Login</button>
+                                    <form>
+                                        <label style={{ color: '#999999' }}><strong>Login</strong></label><br></br>
+                                        <input class="form-control" style={{ width: '100%', marginBottom: '5%' }} type='text' placeholder='Email'></input>
+                                        <input class="form-control" style={{ width: '100%', marginBottom: '3%' }} type='password' placeholder='Password'></input>
+                                        <a style={{ marginRight: '35%', color: '#999999' }} href='#'>Forgot Password?</a>
+                                        <button class='btn btn-primary' type='submit'>Login</button>
                                     </form>
                                 </div>
                                 <p style={{ color: '#999999' }}>
@@ -56,8 +118,8 @@ class login extends Component {
                                 </p>
                             </div>
                         </div>
-                        <div class='col col-sm-12'>
-                            <p style = {{textAlign:'center'}}>
+                        <div class='col col-md-12'>
+                            <p style={{ textAlign: 'center' }}>
                                 New:
                                 <a href='#'>Dutch</a>,&nbsp;
                                 <a href='#'>Danish</a>,&nbsp;
@@ -69,16 +131,16 @@ class login extends Component {
                                 <a href='#'>Tamil</a>
                             </p>
                         </div>
-                        <div class='col col-sm-12' style = {{marginTop:'1%',marginBottom:'1%'}}>
-                            <p style = {{textAlign:'center'}}>
-                                <a style = {{marginRight:'2%',color: '#999999'}} href='#'>About</a>&nbsp;
-                                <a style = {{marginRight:'2%',color: '#999999'}} href='#'>Languages</a>&nbsp;
-                                <a style = {{marginRight:'2%',color: '#999999'}} href='#'>Careers</a>&nbsp;
-                                <a style = {{marginRight:'2%',color: '#999999'}} href='#'>Businesses</a>&nbsp;
-                                <a style = {{marginRight:'2%',color: '#999999'}} href='#'>Privacy</a>&nbsp;
-                                <a style = {{marginRight:'2%',color: '#999999'}} href='#'>Terms</a>&nbsp;
-                                <a style = {{marginRight:'2%',color: '#999999'}} href='#'>Contact</a>
-                                <a style = {{marginRight:'2%',color: '#999999'}} href='#'>Quora Inc. 2019</a>
+                        <div class='col col-md-12'>
+                            <p style={{ textAlign: 'center' }}>
+                                <a style={{ marginRight: '1%', color: '#999999' }} href='#'>About</a>&nbsp;
+                                <a style={{ marginRight: '1%', color: '#999999' }} href='#'>Languages</a>&nbsp;
+                                <a style={{ marginRight: '1%', color: '#999999' }} href='#'>Careers</a>&nbsp;
+                                <a style={{ marginRight: '1%', color: '#999999' }} href='#'>Businesses</a>&nbsp;
+                                <a style={{ marginRight: '1%', color: '#999999' }} href='#'>Privacy</a>&nbsp;
+                                <a style={{ marginRight: '1%', color: '#999999' }} href='#'>Terms</a>&nbsp;
+                                <a style={{ marginRight: '1%', color: '#999999' }} href='#'>Contact</a>
+                                <a style={{ marginRight: '1%', color: '#999999' }} href='#'>Quora Inc. 2019</a>
                             </p>
 
                         </div>
