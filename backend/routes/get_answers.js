0619e825bfc22f22a6df7142d92bc0e2e99442df
Naveen Ravipati <naveenravipati7gmail.com>
2019-04-29 21:56:15 -0700

    Kafka route is used for Get Answers

--- backend/routes/get_answers.js
+++ backend/routes/get_answers.js
@@ -1,13 +1,14 @@
 var Answers = require('../models/Answers');
 var Questions = require('../models/Questions');
 var routerr = require('express').Router();
+var kafka = require('../kafka/client');
+
 
 routerr.post('/get_answers', function (req, res) {
 console.log('=========================Inside Backend - Get Answers module =========================');
 console.log("Object received ", req);
-var result = {}
-Answers.find({question_id:req.body.question_id},function(err,Answer_result){
-    Questions.find({_id:req.body.question_id},function(err,Question_result){
+
+    kafka.make_request("get_answers", req.body, function (err, result) {
     if(err){
         res.writeHead(400, {
             'Content-type': 'application/json'
@@ -15,16 +16,13 @@ Answers.find({question_id:req.body.question_id},function(err,Answer_result){
         res.end('Error in getting Answers');
     }
     else{
-        result = {'question_details':Question_result,'answer_details':Answer_result}
         console.log(result);
         res.writeHead(200, {
             'Content-type': 'application/json'
         });
         res.end(JSON.stringify(result));
     }
-})
-})
-
+    })
 })
 
 module.exports=routerr
